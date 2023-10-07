92f54d76af8211826edd9578e8aba5ff7fef8801
Skillial <jeancabrera2003@gmail.com>
2023-10-07 14:06:29 +0800

    mixed the other js to togglesidebar.js since needs to load everytime

--- public/js/togglesidebar.js
+++ public/js/togglesidebar.js
@@ -45,3 +45,45 @@ function toggleNav() {
         }
     }
 }
+
+
+$(document).on('click', '#logout', function() {
+    fetch('/logout', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    })
+    .then(response => {
+        if (response.ok) {
+            console.log("here")
+            location.href="/";
+        } else {
+            return response.json().then(data => {
+                // var errorDiv = document.getElementById("error");
+                // errorDiv.style.display="block";
+                // errorDiv.textContent=data.error;
+            });
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+    });
+
+});
+
+
+function authCheck(authority){
+    if(authority=="Admin"){
+        console.log(authority)
+    }else if (authority=="SEDO"){
+        console.log(authority)
+    }else{
+        console.log(authority)
+    }
+}
+
+function open(button) {
+    var anchor = document.getElementById(button);
+    anchor.style.color = "rgb(49, 118, 214)";
+}
