645fc30ea873114c18a99ff3d03677e4e3ff8c53
Hannah <hannah_teves@dlsu.edu.ph>
2023-11-10 09:12:58 +0800

    formet currency

--- public/js/formatNumbers.js
+++ public/js/formatNumbers.js
@@ -3,5 +3,6 @@ document.addEventListener('DOMContentLoaded', function () {
     for (let i = 0, len = x.length; i < len; i++) {
         let num = Number(x[i].innerHTML).toLocaleString('en');
         x[i].innerHTML = num;
+        x[i].classList.add("currSign");
     }
 });
