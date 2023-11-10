fe48c1e45e3efc70653ef1f4ca1cb4f7ad76db29
Hannah <hannah_teves@dlsu.edu.ph>
2023-11-10 08:54:14 +0800

    format currency

---
+++ public/js/formatNumbers.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function () {
+    let x = document.querySelectorAll(".format-number");
+    for (let i = 0, len = x.length; i < len; i++) {
+        let num = Number(x[i].innerHTML).toLocaleString('en');
+        x[i].innerHTML = num;
+    }
+});
