e2d234ebe19b9518bfe2657e0393a90da65daa7a
BanzaiNamco <orrin_landon_uy@dlsu.edu.ph>
2023-11-12 02:11:40 +0800

    fix add cluster form validation

--- public/js/addCluster.js
+++ public/js/addCluster.js
@@ -1,51 +1,79 @@
 document.addEventListener('DOMContentLoaded', function () {
-    document.getElementById('addclusterForm').addEventListener('submit', function (event) {
-        event.preventDefault();
-        const formData = new FormData(event.target);
-        const formDataObject = {};
+    (() => {
+        'use strict'
+        const forms = document.querySelectorAll('.needs-validation'); // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        Array.from(forms).forEach(form => { // Loop over them and prevent submission
+            form.addEventListener('submit', event => {
+                if (form.name) { 
+                    addCluster(event.target, form.name);
+                }
+                if (!form.checkValidity()) {
+                    event.preventDefault()
+                    event.stopPropagation()
+                }
+                event.preventDefault()
+                form.classList.add('was-validated')
+            }, false)
+        })
 
-        // Convert FormData to plain object
-        formData.forEach((value, key) => {
-            formDataObject[key] = value;
+        document.getElementById('clusterName').addEventListener('keyup', event => {
+            const formNameInput = event.target;
+            const invalidFeedback = formNameInput.nextElementSibling;
+            formNameInput.setCustomValidity('');
+            formNameInput.classList.remove('is-invalid');
+            formNameInput.classList.remove('is-valid');
+            invalidFeedback.textContent = '';
         });
-        if (formDataObject["name"]=='') {
-            const clusterNameInput = document.getElementById('clusterName');
-            clusterNameInput.setCustomValidity(' ');
-            clusterNameInput.reportValidity();
-            const invalidFeedback = clusterNameInput.nextElementSibling;
-            invalidFeedback.textContent = 'Please enter a cluster name.';
-        }
-        // Check if all values in formDataObject are not empty
-        const fieldEmpty = Object.values(formDataObject).every(value => value !== '');
 
-        if (fieldEmpty) {
-            fetch('/newCluster', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(formDataObject)
-            })
-                .then(response => response.json())
-                .then(data => {
-                    if (data.success) {
-                        window.location.reload();
-                    } else if (data.error) {
-                        const clusterNameInput = document.getElementById('clusterName');
-                        clusterNameInput.setCustomValidity(' ');
-                        clusterNameInput.reportValidity();
-                        const invalidFeedback = clusterNameInput.nextElementSibling;
-                        invalidFeedback.textContent = 'The cluster name is already in use';
-                    }
-                })
-                .catch(error => {
-                    console.error('Error:', error);
-                    // Handle errors here
-                });
-        } else {
-            // Handle case where at least one value is empty
-            console.log('Please fill in all fields.');
-            // You might want to provide user feedback or take appropriate action.
-        }
-    });
+    })()
+    
 });
+
+function addCluster(form, nameInput){
+    const formData = new FormData(form);
+    const formDataObject = {};
+    const formNameInput = nameInput;
+    const invalidFeedback = formNameInput.nextElementSibling;
+
+    // Convert FormData to plain object
+    formData.forEach((value, key) => {
+        formDataObject[key] = value;
+    });
+    if (formDataObject["name"]=='') {
+        formNameInput.setCustomValidity('Invalid field.');
+        formNameInput.classList.add('is-invalid');
+        formNameInput.classList.remove('is-valid');
+        invalidFeedback.textContent = 'Please enter a cluster name.';
+    }
+    // Check if all values in formDataObject are not empty
+    const fieldEmpty = Object.values(formDataObject).every(value => value !== '');
+     
+    if (fieldEmpty) {
+        fetch('/newCluster', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(formDataObject)
+        })
+            .then(response => response.json())
+            .then(data => {
+                if (data.success) {
+                    window.location.reload();
+                } else if (data.error) {
+                    formNameInput.setCustomValidity('Invalid field.');
+                    formNameInput.classList.add('is-invalid');
+                    formNameInput.classList.remove('is-valid');
+                    invalidFeedback.textContent = 'The cluster name is already in use';
+                }
+            })
+            .catch(error => {
+                console.error('Error:', error);
+                // Handle errors here
+            });
+    } else {
+        // Handle case where at least one value is empty
+        console.log('Please fill in all fields.');
+        // You might want to provide user feedback or take appropriate action.
+    }
+}
