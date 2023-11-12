ab0c8f588e7fd0f6cf29a025fc1f30fef4ade9fb
Skillial <jeancabrera2003@gmail.com>
2023-11-13 02:30:35 +0800

    fixed registration

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -8,32 +8,40 @@ const registerController = {
   register: async (req, res) => {
     if (req.session.isLoggedIn) {
       try {
-        const loggedInUser = await User.findById(req.session.userId);
-        if (loggedInUser.authority !== 'Admin') {
-          return res.status(401).json({ error: "Only admins can register new accounts." });
-        }
-        const { username, password, repassword, authority, validPart } = req.body;
-        if (password !== repassword) {
-          return res.status(400).json({ error: "Passwords do not match." });
-        }
-        const MIN_PASSWORD_LENGTH = 6;
-        const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])/; //checking inputs for other stuff
-        if (password.length < MIN_PASSWORD_LENGTH || !passwordRegex.test(password)) {
-          return res.status(400).json({ error: "Password must be at least 6 characters long and must contain upper and lowercase lettes, and numbers." });
-        }
-        const usernameRegex = /^(?=.{3,15}$)(?=.*[a-zA-Z0-9])[a-zA-Z0-9_-]*$/;
-        if (!usernameRegex.test(username)) {
-          return res.status(400).json({ error: "Username must contain at least one letter or number, and be between 3-15 characters long, and cannot be 'visitor'!" });
-        }
+        const { authority, username, password } = req.body;
+        console.log(req.body);
         const existingUser = await User.findOne({ username });
         if (existingUser) {
-          return res.status(400).json({ error: "Username already exists" });
+          return res.json({ error: "Username already exists" });
         }
-        const user = new User({ username, password, authority, validPart });
-        const savedUser = await user.save();
-        savedUser.userId = savedUser._id;
-        await savedUser.save();
-        res.redirect("/success");
+        let newUser;
+        if (authority == "Admin") {
+          newUser = new User({
+            authority,
+            username,
+            password
+          });
+        } else if (authority == "SEDO") {
+          const { validCluster } = req.body
+          newUser = new User({
+            authority,
+            username,
+            password,
+            validCluster
+          });
+        } else {
+          const { validGroup } = req.body
+          newUser = new User({
+            authority,
+            username,
+            password,
+            validGroup
+          });
+        }
+        await newUser.save();
+        if (newUser){
+          res.json({succes: "New user has been added!"})
+        }
       } catch (error) {
         console.error(error);
         return res.status(500).json({ error: "An error occured, please try again" });
