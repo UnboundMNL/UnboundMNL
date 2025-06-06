9b781ca760c0abaa36a49399b96abc4086804ea7
marieltmdg <marielmollytamondong@gmail.com>
2025-06-06 14:08:12 +0800

    feat: parse user details

--- controllers/uploadsController.js
+++ controllers/uploadsController.js
@@ -4,23 +4,36 @@ module.exports = {
     post: async (req, res) => {
         try {
             const workbook = XLSX.readFile(req.file.path);
-            
-            const sheets = {};
+            const sheetName = workbook.SheetNames[0]; 
+            const sheet = workbook.Sheets[sheetName];
 
-            workbook.SheetNames.forEach(sheetName => {
-                const sheet = workbook.Sheets[sheetName];
-                
-                const rows = XLSX.utils.sheet_to_json(sheet, {
-                    header: 1,
-                    defval: '',
-                });
-                
-                sheets[sheetName] = rows;
+            const rows = XLSX.utils.sheet_to_json(sheet, {
+                header: 1,
+                defval: '',
             });
 
-            // TODO: Parse sheets
+            const headerRow = rows[0];
+            const dataRows = rows.slice(1);
+
+            const users = dataRows
+                .filter(row => row[0]) 
+                .map(row => ({
+                    lastName: row[1],              
+                    firstName: row[2],             
+                    status: row[3],           
+                    chid: row[4],              
+                    parentName: row[5],      
+                }));
+
+            // DEBUGGING: Prints the parsed users to the console
+            console.log(users);
+
+            // TODO: Save the users to the database
+            // TODO: Read yearly savings
+
         } catch (err) {
-            // TODO: Error
+            console.error(err);
+            res.status(500).json({ success: false, message: 'Failed to process Excel file.' });
         }
     }
 };
