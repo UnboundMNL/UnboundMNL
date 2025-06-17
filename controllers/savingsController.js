098549546c361357b4b8c1b3e9f2156d1ffd389a
Lorenzo Alfred Nery <61353378+Renzios@users.noreply.github.com>
2025-06-17 20:18:55 +0800

    Revert "Revert "[BACKEND] Bulk registering""

--- controllers/savingsController.js
+++ controllers/savingsController.js
@@ -92,6 +92,9 @@ const savingsController = {
 
                     member.savings.push(newSaving._id);
                     const updatedMember = await member.save();
+
+                    // TODO: Update the group, project, and cluster totals
+                    /*
                     const group = await Group.findById(req.session.groupId);
                     group.totalKaban += (updatedData.totalSaving - currentSaving.totalSaving);
 
@@ -108,7 +111,11 @@ const savingsController = {
                     const updatedCluster = await cluster.save();
                     const updatedSaving = await Saving.findOneAndUpdate({ memberID: id, year }, updatedData, { new: true });
                     if (updatedSaving && updatedMember && updatedGroup && updatedProject && updatedCluster) {
-                        res.json();
+                        res.json({ data: "true"});
+                    }
+                        */
+                    if (updatedSaving && updatedMember) {
+                        res.json({ data: "true"});
                     }
                 }
             } else {
